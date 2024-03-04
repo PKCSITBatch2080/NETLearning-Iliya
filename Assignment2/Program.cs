5eb83f1acf7f83ee2f2e4235e618ce9e9eab795f
iliya03 <iliyafathma@gmail.com>
2024-03-04 06:37:08 +0545

    Movie Booking System

---
+++ Assignment2/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        MovieBookingSystem movieBooking = new MovieBookingSystem("Inception", "Science Fiction", 200);
+        movieBooking.DisplayInfo();
+
+        // Booking seats
+        movieBooking.BookSeats(4);
+        movieBooking.DisplayInfo();
+
+        SpecialMovieBookingSystem specialMovieBooking = new SpecialMovieBookingSystem("Avatar", "Fantasy", 10);
+        specialMovieBooking.DisplayInfo();
+
+        // Booking seats and reserving special seats for the special movie
+        specialMovieBooking.BookSeats(5);
+        specialMovieBooking.ReserveSpecialSeats(2);
+        specialMovieBooking.DisplayInfo();
+    }
+}
